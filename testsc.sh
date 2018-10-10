39e1613f4e5fe3500608115a69f58e1892a34a45
Vadim <glebowadim@gmail.com>
2018-10-10 12:38:23 +0700

    Update

--- testsc.sh
+++ testsc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "test test test master MASTER up after v2 reset 2123 666 after refactoring test test test master MASTER up after v2 reset 2123 666 after refactoring test test test master MASTER up after v2 reset 2123 666 after refactoring test test test master MASTER up after v2 reset 2123 666 after refactoring test test test master MASTER up after v2 reset 2123 666 after refactoring test test test master MASTER up after v2 reset 2123 666 after refactoring"
-echo "test test test"
+echo "test test test 5"
