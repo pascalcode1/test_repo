2daeda6b952bfed87061913b1efc4531729fe423
Vadim <glebowadim@gmail.com>
2020-06-30 14:44:40 +0600

    Update test.py

--- test.py
+++ test.py
@@ -9,3 +9,8 @@ with open('ihub_parameters.json', "rb") as PFile:
 print(data["processId"])
 print(data["ovUrl"])
 print(data["logLevel"])
+
+f = open("guru99.txt","w+")
+for i in range(10):
+    f.write("This is line %d\r\n" % (i+1))
+f.close()
