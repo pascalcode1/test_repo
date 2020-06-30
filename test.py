05111dcc9ef70c8d55f76293b92f3a63a201138b
Vadim <glebowadim@gmail.com>
2020-06-30 14:28:18 +0600

    update test

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
 print('777 111')
 print('777 888 11999')
+
+with open('ihub_parameters.json', "rb") as PFile:
+    data = json.loads(PFile.read().decode('utf-8'))
+
+print(data["processId"])
+print(data["ovUrl"])
+print(data["logLevel"])
