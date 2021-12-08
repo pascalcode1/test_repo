acbc02917e73399e456ff2aee503a6283ed84505
Vadim Glebov <glebowadim@gmail.com>
2021-12-08 12:04:21 +0700

    Update test.sql

--- installation/test.sql
+++ installation/test.sql
@@ -1,5 +1,13 @@
+delete from v_installation_step
+where installation_step_id = 10;
+commit;
+
 insert into v_installation_step (installation_step_id, name)
 values (10, 'test');
+commit;
+
+delete from v_installation_step
+where installation_step_id = 10;
 commit;
 
 select *
