e8803034f4a34f53c8c35766fbc8e0f61b339a49
Vadim Glebov <glebowadim@gmail.com>
2021-12-15 10:16:50 +0700

    Add test1.sql

---
+++ installation/test1.sql
@@ -0,0 +1,59 @@
+delete from v_installation_step
+where installation_step_id = 10;
+commit;
+
+insert into v_installation_step (installation_step_id, name)
+values (10, 'test');
+commit;
+
+delete from v_installation_step
+where installation_step_id = 10;
+commit;
+
+select *
+  from v_installation_step;
+  
+delete from v_installation_step
+where installation_step_id = 10;
+commit;
+
+insert into v_installation_step (installation_step_id, name)
+values (10, 'test');
+commit;
+
+delete from v_installation_step
+where installation_step_id = 10;
+commit;
+
+select *
+  from v_installation_step;
+  
+delete from v_installation_step
+where installation_step_id = 10;
+commit;
+
+insert into v_installation_step (installation_step_id, name)
+values (10, 'test');
+commit;
+
+delete from v_installation_step
+where installation_step_id = 10;
+commit;
+
+select *
+  from v_installation_step;
+  
+delete from v_installation_step
+where installation_step_id = 10;
+commit;
+
+insert into v_installation_step (installation_step_id, name)
+values (10, 'test');
+commit;
+
+delete from v_installation_step
+where installation_step_id = 10;
+commit;
+
+select *
+  from v_installation_step;
