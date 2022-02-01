887bcaf675de6f92d3b435c4a537a8359a6b0007
Vadim Glebov <glebowadim@gmail.com>
2022-02-02 00:49:10 +0700

    Update test.sql

--- installation/test.sql
+++ installation/test.sql
@@ -1,2 +1,8 @@
-insert into v_installation_step (installation_step_id, name)
-values (10, 'test');
+begin 
+    insert into v_installation_step (installation_step_id, name)
+    values (10, 'test');
+    commit;
+    
+    dbms_output.put_line('');
+end;
+/
