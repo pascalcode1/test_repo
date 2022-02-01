32193d17f6d8b6279bc0e0e55450c1d7a95161b8
Vadim Glebov <glebowadim@gmail.com>
2022-02-02 01:45:11 +0700

    Update test.sql

--- installation/test.sql
+++ installation/test.sql
@@ -1,8 +1,17 @@
-begin 
+declare 
+   v_test varchar2(1000);
+begin
+    pkg_sec.set_pid(10009255);
+
     update blob_data 
-       set FILENAME = 'test.jpg' 
+       set FILENAME = 'test999.jpg' 
      where blob_data_id = 10010443155;
     commit;
     
-    dbms_output.put_line('test');
+    select FILENAME
+      into v_test
+      from blob_data
+     where blob_data_id = 10010443155;
+    
+    dbms_output.put_line(v_test);
 end;
