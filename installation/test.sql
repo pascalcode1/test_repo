a3016c7e1f467acf689c778c988d5a3a07b9d9cb
Vadim Glebov <glebowadim@gmail.com>
2022-02-06 20:28:07 +0700

    Update test.sql

--- installation/test.sql
+++ installation/test.sql
@@ -1,28 +1,32 @@
 declare 
-   v_test varchar2(1000);
+   v_test_id number;
 begin
     pkg_sec.set_pid(10009255);
+    
+    dbms_output.put_line('Updating BLOB Data filename');
 
     update blob_data 
-       set FILENAME = 'test999.jpg' 
-     where blob_data_id = 10010443155;
-    commit;
-    
-    select FILENAME
-      into v_test
+       set filename = 'test111.jpg'
+     where filename = 'test999.jpg';
+     
+    select blob_data_id
+      into v_test_id
       from blob_data
-     where blob_data_id = 10010443155;
+     where filename = 'test111.jpg';
     
-    dbms_output.put_line(v_test);
-    dbms_output.put_line(v_test);
-    dbms_output.put_line(v_test);
-    dbms_output.put_line(v_test);
-    dbms_output.put_line(v_test);
+    dbms_output.put_line('Updated BLOB Data with id = ' || to_char(v_test_id));
+    dbms_output.put_line('BLOB Data filename = test111.jpg');
+
+    update blob_data 
+       set filename = 'test999.jpg'
+     where filename = 'test111.jpg';
     
-      execute immediate '    create table filter_opt_xt_restriction_xref444 (
-    filter_opt_id number not null,
-    trackor_type_id number not null,
-    program_id number not null
-     )';
+    select blob_data_id
+      into v_test_id
+      from blob_data
+     where filename = 'test999.jpg';
+     
+    dbms_output.put_line('Updated BLOB Data with id = ' || to_char(v_test_id));
+    dbms_output.put_line('BLOB Data filename = test999.jpg');
 
 end;
