67a0b428500deaa5f1e9fba775483b724088dfaa
Vadim Glebov <glebowadim@gmail.com>
2023-03-23 11:02:29 +0700

    Test script

--- installation/2. test.sql
+++ installation/2. test.sql
@@ -1,5 +1,13 @@
+declare
+    v_id number;
 begin
     
-    dbms_output.put_line('Test'|| chr(13) || chr(10));
+    dbms_output.put_line('Lorem ipsum dolor sit amet, consectetur adipiscing elit.');
+    dbms_output.put_line('Lorem ipsum dolor sit amet, consectetur adipiscing elit.');
+    dbms_output.put_line('Lorem ipsum dolor sit amet, consectetur adipiscing elit.');
 
+    select xitor_type_id
+      into v_id
+      from xitor_type
+     where xitor_type_id = 0;
 end;
