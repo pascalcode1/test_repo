765fddf7f6c24fe26956665e4e2be324a6be256f
Vadim <glebowadim@gmail.com>
2023-05-10 14:32:54 +0700

    Update test.py

--- test.py
+++ test.py
@@ -5,9 +5,10 @@ print('777 888 11999')
 
 a = 0
 b = 1
-# while True:
-#     a = b + 1
-#     print(a)
+while True:
+    a = b + 1
+    print("Hello, World!", flush=True)
+   
 
 with open('ihub_parameters.json', "rb") as PFile:
     data = json.loads(PFile.read().decode('utf-8'))
