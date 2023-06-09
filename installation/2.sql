d5b51ad8e7dbe14902bc3c2558a14f1a0078e10d
Vadim <glebowadim@gmail.com>
2023-06-09 09:01:18 +0700

    Update 2.sql

--- installation/2.sql
+++ installation/2.sql
@@ -1,16 +1,4 @@
 begin
     
-    dbms_output.put_line('Test');
-    dbms_output.put_line('Test');
-    dbms_output.put_line('Test');
-    dbms_output.put_line('Test');
-    
-    declare
-        v_test number;
-    begin
-        select xitor_id
-          into v_test
-          from xitor
-         where xitor_id = -1;
-    end;
+    dbms_outp
 end;
