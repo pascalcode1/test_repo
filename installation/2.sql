a8593770531a014e0a6877156174db3bcb3b1c18
Vadim <glebowadim@gmail.com>
2023-06-09 08:39:29 +0700

    Update 2.sql

--- installation/2.sql
+++ installation/2.sql
@@ -4,6 +4,6 @@ begin
     dbms_output.put_line('Test'|| chr(13) || chr(10));
     dbms_output.put_line('Test'|| chr(13) || chr(10));
     dbms_output.put_line('Test'|| chr(13) || chr(10));
-
+    pkg_sec.test();
 
 end;
