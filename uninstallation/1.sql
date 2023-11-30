6a3bbd3c0bdccea1413a739983f9f09c927eee3e
Vadim Glebov <glebowadim@gmail.com>
2023-11-30 21:12:30 +0700

    Update uninstall sql script

--- uninstallation/1.sql
+++ uninstallation/1.sql
@@ -1,3 +1,12 @@
+declare
+    v_comp_package_id number;
 begin
+    select components_package_id
+      into v_comp_package_id
+      from components_package
+     where name = 'TEST1';
+
+    pkg_comp_package.delete_comp_package(v_comp_package_id);
+
     dbms_output.put_line('All Module Components have been successfully removed.'|| chr(13) || chr(10));
 end;
