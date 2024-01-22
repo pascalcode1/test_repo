b1232f10670b709a671089d13e75f2e740d4752a
Vadim <glebowadim@gmail.com>
2024-01-23 00:08:38 +0700

    Update 2.sql

--- installation/2.sql
+++ installation/2.sql
@@ -6,12 +6,11 @@ exception
     when others then
         for v_rec in (select objref_validation_error_id,
                              object_name,
-                             replace(error_msg, chr(13) || chr(10), '') as error_msg,
-                             replace(object_ddl_text, chr(13) || chr(10), '') as object_ddl_text
+                             replace(error_msg, chr(13) || chr(10), '') as error_msg
                         from objref_validation_error) loop
             dbms_output.put_line('objref_validation_error_id:' || v_rec.objref_validation_error_id
                 || chr(13) || chr(10) || 'object_name:' || v_rec.object_name
-                || chr(13) || chr(10) || 'error_msg:' || v_rec.error_msg
-                || chr(13) || chr(10) || 'object_ddl_text:' || v_rec.object_ddl_text);
+                || chr(13) || chr(10) || 'error_msg:' || v_rec.error_msg);
         end loop;
+        raise;
 end;
