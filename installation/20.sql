2062df00a249023564107bd0222387bd3c4f02da
Vadim Glebov <glebowadim@gmail.com>
2024-02-08 21:55:15 +0700

    Fix smt

--- installation/20.sql
+++ installation/20.sql
@@ -1,6 +1,6 @@
 begin
     update config_field
-       set obj_xitor_type_id = util_module.get_module_param_val('test_repo', 'TrackorTypeID');
+       set obj_xitor_type_id = util_module.get_module_param_val('test_repo', 'TrackorTypeID')
      where config_field_name = 'T1_TRACKOR_SELECTOR';
 
     dbms_output.put_line('Selector''s Trackor Type has been updated');
