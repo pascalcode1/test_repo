753c165e1ab3983988c76265eaa30c22dba408c4
Vadim Glebov <glebowadim@gmail.com>
2024-02-14 02:37:01 +0700

    Add new files to sample data

--- installation/20.sql
+++
@@ -1,7 +0,0 @@
-begin
-    update config_field
-       set obj_xitor_type_id = util_module.get_module_param_val('test_repo', 'TrackorTypeID')
-     where config_field_name = 'T1_TRACKOR_SELECTOR';
-
-    dbms_output.put_line('Selector''s Trackor Type has been updated');
-end;
