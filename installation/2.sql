f934a5da1d17039149b45d39863d2b4df07ac8ed
Vadim Timofeev <pascalcode1@gmail.com>
2024-04-19 09:40:15 +0500

    kek

--- installation/2.sql
+++ installation/2.sql
@@ -1,5 +1,10 @@
+declare
+    v_module_name varchar(100);
 begin
-    
-    dbms_output.put_line('Test'|| chr(13) || chr(10));
+    select module_name
+    into v_module_name
+    from module
+    where module_id = :p_module_id;
 
+    dbms_output.put_line(v_module_name);
 end;
