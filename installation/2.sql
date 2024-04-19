449767af63c1912d7d49063e5241b4448b2a1a83
Vadim Timofeev <pascalcode1@gmail.com>
2024-04-19 10:01:17 +0500

    test

---
+++ installation/2.sql
@@ -0,0 +1,10 @@
+declare
+    v_module_name varchar(100);
+begin
+    select module_name
+    into v_module_name
+    from module
+    where module_id = :p_module_id;
+
+    dbms_output.put_line('name: ' || v_module_name);
+end;
