ea850659448d6b46cd375674a1714d5ea3fa4f21
Vadim Timofeev <pascalcode1@gmail.com>
2024-07-04 15:14:43 +0200

    test

--- uninstallation/1.sql
+++ uninstallation/1.sql
@@ -1,3 +1,7 @@
 begin
+    dbms_output.put_line('param1: ' || :name1);
+    dbms_output.put_line('param2: ' || :name2);
+    dbms_output.put_line('param3: ' || :name3);
     dbms_output.put_line('Uninstallation... Id of current module: ' || :p_module_id);
+    util_module.delete_module_packages(:p_module_id);
 end;
