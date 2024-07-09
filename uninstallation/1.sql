d21d359f7763aac0c032abb9da5d06b89134bb5a
Vadim Timofeev <pascalcode1@gmail.com>
2024-07-09 14:55:30 +0200

    params test

--- uninstallation/1.sql
+++ uninstallation/1.sql
@@ -1,7 +1,17 @@
+declare
+    param_1 module_param.value%type;
+    param_2 module_param.value%type;
+    param_3 module_param.value%type;
 begin
-    dbms_output.put_line('param1: ' || :name1);
-    dbms_output.put_line('param2: ' || :name2);
-    dbms_output.put_line('param3: ' || :name3);
+    select util_module.get_module_param_val('test_repo', 'name1') into param_1 from dual;
+    select util_module.get_module_param_val('test_repo', 'name2') into param_2 from dual;
+    select util_module.get_module_param_val('test_repo', 'name3') into param_2 from dual;
+
+    dbms_output.put_line('The entered parameters will be used for uninstallation:');
+    dbms_output.put_line('param1: ' || param_1);
+    dbms_output.put_line('param2: ' || param_2);
+    dbms_output.put_line('param3: ' || param_3);
+
     dbms_output.put_line('Uninstallation... Id of current module: ' || :p_module_id);
     util_module.delete_module_packages(:p_module_id);
 end;
