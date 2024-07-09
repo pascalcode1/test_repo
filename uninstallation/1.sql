e784ae725d163d8eb7e887ecd472edd922792f10
Vadim Timofeev <pascalcode1@gmail.com>
2024-07-09 15:55:12 +0200

    params test

--- uninstallation/1.sql
+++ uninstallation/1.sql
@@ -3,9 +3,9 @@ declare
     param_2 module_param.value%type;
     param_3 module_param.value%type;
 begin
-    select util_module.get_module_param_val('test_repo', 'name1') into param_1 from dual;
-    select util_module.get_module_param_val('test_repo', 'name2') into param_2 from dual;
-    select util_module.get_module_param_val('test_repo', 'name3') into param_2 from dual;
+    select util_module.get_module_param_val('test_repo', 'UNinstallationParam1') into param_1 from dual;
+    select util_module.get_module_param_val('test_repo', 'UNinstallationParam2') into param_2 from dual;
+    select util_module.get_module_param_val('test_repo', 'UNinstallationParam3') into param_2 from dual;
 
     dbms_output.put_line('The entered parameters will be used for uninstallation:');
     dbms_output.put_line('param1: ' || param_1);
